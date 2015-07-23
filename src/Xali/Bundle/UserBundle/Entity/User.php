409413c6072dcf5b9b4c588c62d7c2883918fb88
anthonybocci <boccianthony@yahoo.fr>
2015-07-23 16:07:08 +0200

    Add csrf token in login Fix a typo in User entity

--- src/Xali/Bundle/UserBundle/Entity/User.php
+++ src/Xali/Bundle/UserBundle/Entity/User.php
@@ -58,7 +58,7 @@ class User implements UserInterface
      * @var string
      * 
      * @ORM\Column(name="firstname", type="string", length=100) 
-     * @Asset\NotBlank()
+     * @Assert\NotBlank()
      */
     private $firstname;
     
