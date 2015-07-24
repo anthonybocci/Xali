f82cd7d39062962427ff5d7284c7f3d07e752537
anthonybocci <boccianthony@yahoo.fr>
2015-07-24 14:13:53 +0200

    Use converter service for height and weight in add_survivorAction

--- src/Xali/Bundle/SurvivorBundle/Controller/SurvivorController.php
+++ src/Xali/Bundle/SurvivorBundle/Controller/SurvivorController.php
@@ -15,6 +15,8 @@ class SurvivorController extends Controller
 {
     /**
      * Add a survivor
+     * 
+     * @author Anthony Bocci <boccianthony@yahoo.fr>
      */
     public function add_survivorAction()
     {
@@ -22,17 +24,38 @@ class SurvivorController extends Controller
         $form = $this->createForm(new SurvivorType(), $survivor);
         $request = $this->get('request');
         $entityManager = $this->getDoctrine()->getManager();
+        $converter = $this->container->get('xali_survivor.converter');
         
         //If form is submitted
         if ($request->getMethod() == "POST") {
             $form->handleRequest($request);
             if ($form->isValid()) {
+                $this->convertWeightAndHeight($survivor, $converter);
                 $entityManager->persist($survivor);
                 $entityManager->flush();
             }
         }
         return $this->render('XaliSurvivorBundle:Management:add_survivor.html.twig',
                 array('form' => $form->createView()));
+    }
+    
+    /**
+     * Convert weight and height if necessary
+     * 
+     * @param \Xali\Bundle\SurvivorBundle\Entity\Survivor $survivor
+     * @param \Xali\Bundle\SurvivorBundle\Converter\Converter $converter
+     * @author Anthony Bocci <boccianthony@yahoo.fr>
+     */
+    private function convertWeightAndHeight($survivor, $converter)
+    {
+        //If weight unit is kilogrammes
+        if ($survivor->getWeightUnit() == "kg") {
+            $survivor->setWeight($converter->fromKgToLb($survivor->getWeight()));
+        }
+        //If height unit is centimeters
+        if ($survivor->getHeightUnit() == "cm") {
+            $survivor->setHeight($converter->fromCmToInch($survivor->getHeight()));
+        }
     }
     
     public function see_profileAction()
