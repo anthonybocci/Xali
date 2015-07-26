3d46bbfc139733f281c736eb3a30fd2692b45aee
anthonybocci <boccianthony@yahoo.fr>
2015-07-26 22:17:51 +0200

    Complete actions in campController and display rights informations in views

--- src/Xali/Bundle/CampBundle/Controller/CampController.php
+++ src/Xali/Bundle/CampBundle/Controller/CampController.php
@@ -5,6 +5,7 @@ namespace Xali\Bundle\CampBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Xali\Bundle\CampBundle\Entity\Camp;
 use Xali\Bundle\CampBundle\Form\CampType;
+use Xali\Bundle\OrganisationBundle\Entity\Organisation;
 
 /**
  * Manage camps
@@ -16,9 +17,10 @@ class CampController extends Controller
     /**
      * Add a camp
      * 
+     * @param Xali\Bundle\OrganisationBundle\Entity\Organisation $organisation
      * @author Anthony Bocci <boccianthony@yahoo.fr>
      */
-    public function add_campAction()
+    public function add_campAction(Organisation $organisation)
     {
         $camp = new Camp();
         $form = $this->createForm(new CampType, $camp);
@@ -28,29 +30,62 @@ class CampController extends Controller
         if ($request->getMethod() == "POST") {
             $form->handleRequest($request);
             if ($form->isValid()) {
-                //TMP while idorganisation param is not set in routing
-                $tmpOrg = $em->getRepository('XaliOrganisationBundle:Organisation')->find(1);
-                $camp->setOrganisation($tmpOrg);
-                
+                $camp->setOrganisation($organisation);
                 $em->persist($camp);
                 $em->flush();
             }
         }
-        
-        
         $render = 'XaliCampBundle:Management:add_camp.html.twig';
         return $this->render($render, array(
-                                        'form' => $form->createView()
+                                     'form' => $form->createView(),
+                                     'organisationId' => $organisation->getId(),
                             ));
     }
     
-    public function profileAction()
+    /**
+     * Display a camp's profile
+     * 
+     * @param Xali\Bundle\CampBundle\Entity\Camp $camp
+     * @author Anthony Bocci <boccianthony@yahoo.fr>
+     */
+    public function profileAction(Camp $camp)
     {
-        return $this->render('XaliCampBundle:Profile:profile.html.twig');
+        return $this->render('XaliCampBundle:Profile:profile.html.twig', array(
+                                                                'camp' => $camp,
+                            ));
     }
     
-    public function assign_volunteerAction()
+    /**
+     * Assign a volunteer to a camp
+     * 
+     * @param Xali\Bundle\CampBundle\Entity\Camp $camp
+     * @author Anthony Bocci <boccianthony@yahoo.fr>
+     */
+    public function assign_volunteerAction(Camp $camp)
     {
-        return $this->render('XaliCampBundle:Management:assign_volunteer.html.twig');
+        $insert = null;
+        $request = $this->get('request');
+        $session = $this->get('session');
+        $em = $this->getDoctrine()->getManager();
+        $userRepository = $em->getRepository('XaliUserBundle:User');
+        //If form is submitted and tokens and email exist
+        if ($request->getMethod() == "POST" &&
+                            !empty($session->get('csrf_token')) &&
+                            !empty($request->request->get('csrf_token')) &&
+                            !empty($request->request->get('volunteer_email'))
+        ) {
+            $receivedToken = $request->request->get('csrf_token');
+            //If received token equals to token in session
+            if ($receivedToken == $session->get('csrf_token')) {
+                $volunteer = $userRepository->findOneByEmail(
+                                    $request->request->get('volunteer_email'));
+                //Assign user if email is valid
+                $insert = $userRepository->updateCamp($volunteer, $camp);
+            }
+        }
+        //Set csrf token
+        $session->set('csrf_token', sha1(time() * rand()));
+        $render = 'XaliCampBundle:Management:assign_volunteer.html.twig';
+        return $this->render($render, array('camp' => $camp, 'insertSuccess' => $insert));
     }
 }
