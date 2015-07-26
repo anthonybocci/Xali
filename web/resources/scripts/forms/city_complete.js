21032459490e4667b7df631098b3bb431591f363
anthonybocci <boccianthony@yahoo.fr>
2015-07-26 14:08:53 +0200

    Fix a bug with cities/countries suggestions

--- web/resources/scripts/forms/city_complete.js
+++ web/resources/scripts/forms/city_complete.js
@@ -1,8 +1,9 @@
-var input = document.getElementById("maps");
-var autocomplete = new google.maps.places.Autocomplete(input,{types:['(cities)']});
-google.maps.event.addListener(autocomplete,'place_changed',function(){
-    var place = autocomplete.getPlace();
-    console.log(place);
-    $("#maps").val(place.address_components[1]['short_name']);
-    return;
-});
+function initialize() {
+   var options = {
+  types: ['(cities)'],
+  componentRestrictions: {country: "us"}
+ };
+      var input = document.getElementById('maps');
+      var autocomplete = new google.maps.places.Autocomplete(input);
+   }
+   google.maps.event.addDomListener(window, 'load', initialize);
